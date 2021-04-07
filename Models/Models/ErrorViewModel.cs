6d8453ac331d64f16467a482fecfaa3926b3a5fe
JacobEhrman91 <77745624+JacobEhrman91@users.noreply.github.com>
2021-04-07 03:42:30 -0700

    Add files via upload

---
+++ Models/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PickItCore.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
